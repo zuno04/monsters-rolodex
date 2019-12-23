61d9f9c1ea871db44956da79c01dcfcc0aa6668e
EMAGA NGAMO ALAIN CESAR <emaga87@gmail.com>
2019-12-23 12:27:25 +0100

    Updates

---
+++ precache-manifest.c79222d1cd5d1f763411914e8da765ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8725178688dc75ad68db3578f94a5941",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7153d9745a487612efa9",
+    "url": "/monsters-rolodex/static/css/main.cfeb130e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7153d9745a487612efa9",
+    "url": "/monsters-rolodex/static/js/main.44376802.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
